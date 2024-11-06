dde93e6ffda9cae42c0ef7e5d4c8ed2a78a8988d
Nuhin <153355458+ShanjidulNuhin@users.noreply.github.com>
2024-11-07 04:52:53 +0600

    Add files via upload

---
+++ Main/Main/Account.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main
+{
+    internal class Account
+    {
+        private string _acName;
+        private string _acID;
+        private double _acBalance;
+
+        internal Account() { }
+        internal Account(string acName, string acID, double acBalance)
+        {
+            _acName = acName;
+            _acID = acID;
+            _acBalance = acBalance;
+        }
+        public string AcName
+        {
+            set { _acName = value; }
+            
+        }
+        public string AcID
+        {
+            set { _acID = value; }
+            
+        }
+        public double AcBalance
+        {
+            set { _acBalance = value; }
+        }
+        internal void deposit(double amount)
+        {
+            if (_acBalance < 0)
+            {
+                Console.WriteLine(amount+ " Cannot possible \n");
+            }
+            else
+            {
+                _acBalance += amount;
+                Console.WriteLine(amount+" has been deposit \n");
+            }
+
+        }
+        internal void withdraw(double amount)
+        {
+            if (_acBalance < amount)
+            {
+                Console.WriteLine("You can not Withdraw "+amount);
+                Console.WriteLine();
+            }
+            else
+            {
+                _acBalance -= amount;
+                Console.WriteLine("You can withdraw " + amount);
+                Console.WriteLine();
+            }
+        }
+        internal void transfer(double amount, Account receiver)
+        {
+            if (_acBalance < amount)
+            {
+                Console.WriteLine("You cannot transfer\n");
+            }
+            else
+            {
+                _acBalance -= amount;
+                receiver._acBalance += amount;
+
+                Console.WriteLine("You can transfer " +amount);
+                Console.WriteLine();
+            }
+        }
+        public void ShowBalance(Account a)
+        {
+            Console.WriteLine(_acBalance+" is Your current balance\n");
+        }
+
+    }
+
+}
---
+++ Main/Main/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Account a = new Account("Shanjidul Islam","2550", 5000);
+
+            a.ShowBalance(a);
+            a.deposit(2000000);
+            a.ShowBalance(a);
+            a.withdraw(1500);
+            a.ShowBalance(a);
+            a.withdraw(20000);
+            a.ShowBalance(a);
+
+            Account a2 = new Account();
+
+            a.AcName = "Nam nai";
+            a.AcID = "2012";
+            a.AcBalance = 20000;
+
+            a2.ShowBalance(a2);
+            a2.deposit(405000);
+            a2.ShowBalance(a2);
+            a2.withdraw(7500000);
+            a2.ShowBalance(a2);
+
+            a.transfer(4560, a2);
+            a.ShowBalance(a);
+            a2.ShowBalance(a2);
+
+            a.transfer(80, a2);
+            a.ShowBalance(a);
+            a2.ShowBalance(a2);
+
+           
+
+            Console.ReadKey();
+        }
+
+    }
+}
