a0660d48945d1ea4e81bb79be50b50ef4326d709
Nuhin <153355458+ShanjidulNuhin@users.noreply.github.com>
2024-11-13 09:38:51 +0600

    Add files via upload

---
+++ ConstantLab/ConstantLab/Bank.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstantLab
+{
+    internal class Bank
+    {
+        const int DailyTransactionLimt = 5000;//entity constant
+        readonly int CreditLimit=40000;//run time constant(Value declear optional)
+
+        public Bank(int c1)
+        {
+            this.CreditLimit = c1;
+        }
+        static void Main(string[] args)
+        {
+            Bank b1=new Bank(10000);
+            Bank b2 = new Bank(2000);
+            Bank b3 = new Bank(3000);
+
+            Console.WriteLine("Daily Transaction Limt: "+ Bank.DailyTransactionLimt);
+            Console.WriteLine(b1.CreditLimit);
+            Console.WriteLine(b2.CreditLimit);
+            Console.WriteLine(b3.CreditLimit);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConstantLab/ConstantLab/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstantLab
+{
+    internal class Program
+    {
+        const int num = 100;//class mem.
+        int num2;//instance mem.
+        static int num3;//class mem.
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.num2 = 10;//instance ref
+
+            Program.num3 = 50;//class ref
+            Console.WriteLine(Program.num);
+            Console.WriteLine(Program.num3);
+            Program.num3 = 10;//static value change through the class
+            Console.WriteLine(Program.num3);
+            
+            Console.ReadKey();
+        }
+    }
+}
