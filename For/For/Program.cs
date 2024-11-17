d874df0f1fbcf869a2f3f23d1a1679984f378ece
Nuhin <153355458+ShanjidulNuhin@users.noreply.github.com>
2024-11-18 01:36:26 +0600

    Add files via upload

---
+++ For/For/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            Console.Write("Enter Collumn:");
+            double n =Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter Row:");
+            double m=Convert.ToDouble(Console.ReadLine());
+            for (i = 1; i <= n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write(" ");
+                }
+                for(int k=1; k <= i; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Switch/Switch/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number between 1 to 7");
+            double day=Convert.ToDouble(Console.ReadLine());
+
+            switch (day) 
+            {    
+                case 1: Console.WriteLine("Sater Day");
+                    break;
+                case 2: Console.WriteLine("Sunday");
+                    break;
+                case 3: Console.WriteLine("Monday");
+                    break;
+                case 4: Console.WriteLine("Tuesday");
+                    break;
+                case 5: Console.WriteLine("Wednesday");
+                    break;
+                case 6: Console.WriteLine("Thursday");
+                    break;
+                case 7: Console.WriteLine("Friday");
+                    break;
+                    default: Console.WriteLine("Invalid number");
+                    break;
+            } 
+            Console.ReadKey();
+        }
+    }
+}
